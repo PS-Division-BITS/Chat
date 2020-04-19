d51d87060fdd61befda46ba457694ab5f2315647
chetan22 <ck.chetan20@gmail.com>
2020-04-20 02:56:44 +0530

    fixed consumers.py bug

--- chat_app/consumers.py
+++ chat_app/consumers.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core import serializers
 
 import json
+import jwt
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
@@ -10,7 +12,7 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        print('joining room group')
+
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -27,23 +29,22 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # receive message from WebSocket
-        def receive(self, text_data=None, bytes_data=None):
-            # request from websocket is received here
-            text_data_json = json.loads(text_data)
-            User = get_user_model()
-            sender = serializers.serialize('json', User.objects.filter(id=2))
-            sender = list(User.objects.filter(id=2).values('username'))
-            message = text_data_json['message']
-            
-            # sends out an event to the group having name `self.room_group_name`
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'chat_message',
-                    'sender':  sender[0],
-                    'message': message
-                }
-            )
+    def receive(self, text_data=None, bytes_data=None):
+        # request from websocket is received here
+        text_data_json = json.loads(text_data)
+        User = get_user_model()
+        sender = serializers.serialize('json', User.objects.filter(id=2))
+        sender = list(User.objects.filter(id=2).values('username'))
+        message = text_data_json['message']
+        # sends out an event to the group having name `self.room_group_name`
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'sender':  sender[0],
+                'message': message
+            }
+        )
 
     # recieve message from room_group
     def chat_message(self, event):
