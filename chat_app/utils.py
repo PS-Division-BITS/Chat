f423652fbb429319a781f0e70e522af8afcce04f
chetan22 <ck.chetan20@gmail.com>
2020-05-01 18:46:22 +0530

    Added utility functions -- may remove later

--- chat_app/utils.py
+++ chat_app/utils.py
@@ -1,6 +1,10 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 import jwt
+
+
+User = get_user_model()
 
 
 def get_jwt(payload, secret=settings.SECRET_KEY, algorithm='HS256'):
@@ -12,3 +16,15 @@ def decode_jwt(token, secret=settings.SECRET_KEY, algorithms=['HS256',]):
 def verify_jwt(token, username):
     payload = decode_jwt(token)
     return payload['username'] == username
+
+def is_verified_user(user=None, username=None):
+    assert user and username is not None
+    user = user or User.objects.get(username=username)
+    return user.is_verified
+
+def is_reserved_username(username):
+    assert username
+    for reserved in settings.RESERVED_USERNAMES:
+        if reserved.casefold() == username.casefold():
+            return True
+    return False
