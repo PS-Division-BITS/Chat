b921001552673d08cadc12080082062a8c2c8c55
chetan22 <ck.chetan20@gmail.com>
2020-05-01 14:58:37 +0530

    Added restricted host settings in websockets
    Only hosts defined in ALLOWED_HOSTS setting will be allowed to open websockets

--- Chat/routing.py
+++ Chat/routing.py
@@ -1,11 +1,15 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 import chat_app.routing
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat_app.routing.websocket_urlpatterns
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat_app.routing.websocket_urlpatterns
+            )
         )
     )
 })
